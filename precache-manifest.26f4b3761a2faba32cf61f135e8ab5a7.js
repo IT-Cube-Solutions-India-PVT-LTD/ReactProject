a873c021d667204fc1b2880692a28fc9da830159
Prakhar Agarwal <prakhar.agarwal@dev.itcube.net>
2020-06-14 14:33:00 +0530

    Updates

---
+++ precache-manifest.26f4b3761a2faba32cf61f135e8ab5a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca00a660bdb6b03e4e6f9885dc012a0",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "4207de0552fc86c12041",
+    "url": "/ReactProject/static/css/main.3705eee4.chunk.css"
+  },
+  {
+    "revision": "d5a658b4458ba601d96a",
+    "url": "/ReactProject/static/js/2.a36c009f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/ReactProject/static/js/2.a36c009f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4207de0552fc86c12041",
+    "url": "/ReactProject/static/js/main.fe7975c6.chunk.js"
+  },
+  {
+    "revision": "28cce9c1206385828e0c",
+    "url": "/ReactProject/static/js/runtime-main.b9be4dbc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/ReactProject/static/media/image.d7265326.png"
+  }
+]);
